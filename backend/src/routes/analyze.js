77818e3ed54848d83515ac45b3318dddc73bca9a
Jayprox <jayadeguzman@gmail.com>
2025-08-09 12:57:29 -0700

    service file changes

--- backend/src/routes/analyze.js
+++ backend/src/routes/analyze.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { analyzeText } = require('../services/service');
+const { analyzeText } = require('../services/sentimentService');
 
 router.post('/', async (req, res) => {
   const { sentence } = req.body;
